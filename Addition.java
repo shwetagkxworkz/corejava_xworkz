d9d55765f88871d35a44910f26639cbb7871d2d6
varunkumarxworkz <varunkumarxworz@gmail.com>
2023-04-20 12:22:17 +0530

    methods

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Addition{
+public static void main(String addition[]){
+System.out.println("main started");
+add(345,456);
+add(256,123);
+add(225,225);
+add(500,500);
+System.out.println("main ended");
+}
+
+public static void add(int a, int b){
+System.out.println(a+b);
+}
+}
